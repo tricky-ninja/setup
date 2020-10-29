f5fa4db8cc9d66812441e9bdef31bba29e73141c
Xhackers7 <nishaajithkj@gmail.com>
2020-10-29 12:42:08 +0530

    Updated first.sh

--- first.sh
+++ first.sh
@@ -1,7 +1,17 @@
+
+
+
+
 echo Welcome to first made by Sreyas. This is a script that automates the process of setting up termux. Stay back and enjoy. 
-sleep 5
-clear
-cat banner.txt
+sleep 5		# wait for 5 seconds
+clear	
+cat banner.txt		# read banner.txt
+
+
+
+# installs some packages
+
+
 pkg update && pkg upgrade -y 
 pkg install git 
 pkg install python 
@@ -12,11 +22,11 @@ pkg install ruby
 pkg install wget 
 echo Basic installation donee. Starting to customize termux 
 sleep 8
-git clone https://github.com/Bhai4You/Termux-Banner
+git clone https://github.com/Bhai4You/Termux-Banner	# clones into the given link
 cd Termux-Banner 
 ls
-chmod +x requirement.sh 
-chmod +x t-ban.sh 
-bash requirement.sh
-bash t-ban.sh
+chmod +x requirement.sh 	# gives permison to requirement.sh
+chmod +x t-ban.sh 	#gives permision to t-ban.sh
+bash requirement.sh	#runs requirement.sh
+bash t-ban.sh		#runs t-ban.sh
 
